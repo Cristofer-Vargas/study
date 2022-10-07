121cc0f3364a97da7a61373f3b4d2d0069367300
Cristofer Vargas <vargascristofer97@gmail.com>
2022-10-07 09:55:38 -0300

    first commit

---
+++ 001/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>001</title>
+
+    <link rel="stylesheet" href="001.css">
+
+</head>
+<body>
+
+    <h1>
+        Bom dia
+    </h1>
+
+    <p>
+        Começando...
+    </p>
+
+    <?php
+
+        echo "Olá mundo PHP";
+
+    ?>
+
+</body>
+</html>
