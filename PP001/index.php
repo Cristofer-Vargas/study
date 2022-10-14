65238e0e4abda3ea7ea6e411b16ab23d402a33ff
Cristofer Vargas <vargascristofer97@gmail.com>
2022-10-14 11:53:01 -0300

    PP001

---
+++ PP001/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pessoal Project 001</title>
+
+    <link rel="stylesheet" href="estilo.css">
+
+</head>
+<body>
+    <header>
+        <div id="COlorHeader">
+            <div>
+                <div>
+                    <!-- tr são para definir as linhas da tabela -->
+                    <tr>
+                        <!-- Th são para definir as colunas da tabela -->
+                        <th id="EventHomePage">Home Page</th>
+                        <th id="EventHomePerfil">Perfil</th>
+                    </tr>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div id="MainContain">
+        <div id="PColorContain">
+            <table id="PTableContain">
+                <tr>
+                    <th>
+                        <div class="PContain">
+                            <div id="Tit">
+                                <h1>
+                                    Olá mundo!
+                                </h1>
+                            </div>
+                            <div id="Text">
+                                <p>
+                                    Este é um teste de programação com php, qualquer forma, estilo ou cor nesta página 
+                                    é meramente para experiência
+                                </p>
+                            </div>
+                        </div>
+                    </th>
+                    <th>
+                        <img src="mundo.png" id="PImgContain">
+                    </th>
+                </tr>    
+            </table>
+
+            <p class="PSubContain">
+                Ola pessual, esse parágrafo é um exemplo de formatação neste bloco
+            </P>
+        </div>
+        <div id="SColorContain">
+            <div class="SContain">
+                <div id="Tit">
+                    <h1>
+                        Olá mundo!
+                    </h1>
+                </div>
+                <div id="Text">
+                    <p>
+                        Este é um teste de programação com php, qualquer forma, estilo ou cor nesta página 
+                        é meramente para experiência
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div id="TColorContain">
+            <div class="TContain">
+                <div id="Tit">
+                    <h1>
+                        Olá mundo!
+                    </h1>
+                </div>
+                <div id="Text">
+                    <p>
+                        Este é um teste de programação com php, qualquer forma, estilo ou cor nesta página 
+                        é meramente para experiência
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div>
+            <p>
+                &copy Cristofer Vargas de Santis
+            </p>
+            <p>
+                <img src="InstragramIcon.png" id="IconIns"> cristofer.java
+            </p>
+        </div>
+    </footer>
+</body>
+</html>
