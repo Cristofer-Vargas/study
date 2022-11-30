77c86874c6fd0e7fd1e32766ee07bddc6f1ac90b
Cristofer Vargas <vargascristofer97@gmail.com>
2022-11-30 17:18:15 -0300

    Aulas e exercicios

---
+++ Curso Exercicios/Códigos Curso TI/Carro e Bicicleta/script/script.js
@@ -0,0 +1,7 @@
+function ClicarBuyCar() {
+    window.location.href = "http://www.devmedia.com.br";
+}
+
+function ClicarBuyBic() {
+    window.location.href = "https://www.devmedia.com.br/javascript-redirect-redirecionando-o-usuario-com-window-location/39809";
+}
