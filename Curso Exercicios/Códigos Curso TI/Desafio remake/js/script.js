b69c5bb5fd44fb1715797581468518670af0dd1d
Cristofer Vargas <84339046+Cristofer-Vargas@users.noreply.github.com>
2022-12-14 21:45:34 -0300

    Add files via upload

---
+++ Curso Exercicios/Códigos Curso TI/Desafio remake/js/script.js
@@ -0,0 +1,8 @@
+function ClicarBtn() {
+  var check = document.getElementById('visible');
+  var bottons = document.getElementsByClassName('btn-input');
+  
+  check.style.display = 'block';
+  bottons.style.display = 'none';
+
+}
